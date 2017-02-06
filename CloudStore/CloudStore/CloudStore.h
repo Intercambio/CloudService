2a9a96443969631663e4abc607219cdb8847e565
Tobias Kräntzer <info@tobias-kraentzer.de>
2017-02-06 12:55:53 +0100

    Initial Commit

---
+++ CloudStore/CloudStore/CloudStore.h
@@ -0,0 +1,19 @@
+//
+//  CloudStore.h
+//  CloudStore
+//
+//  Created by Tobias Kraentzer on 06.02.17.
+//  Copyright © 2017 Tobias Kräntzer. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for CloudStore.
+FOUNDATION_EXPORT double CloudStoreVersionNumber;
+
+//! Project version string for CloudStore.
+FOUNDATION_EXPORT const unsigned char CloudStoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CloudStore/PublicHeader.h>
+
+
